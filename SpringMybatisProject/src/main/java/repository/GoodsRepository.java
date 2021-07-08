d2b8fec90700b4bc09ef1deaffa058faa6bc53ab
vip04 <vip04@DESKTOP-PVBN19R>
2021-07-09 02:12:26 +0900

    20210709

---
+++ SpringMybatisProject/src/main/java/repository/GoodsRepository.java
@@ -0,0 +1,28 @@
+package repository;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import model.GoodsDTO;
+
+public class GoodsRepository {
+	@Autowired
+	SqlSession sqlSession;
+	String namespace = "mappers.goodsMapper";
+	String statement;
+	public List<GoodsDTO> goodsList(){
+		statement = namespace + ".goodsList";
+		return sqlSession.selectList(statement);
+	}
+	
+	public void goodsWrite(GoodsDTO dto) {
+		statement = namespace + ".goodsWrite";
+		sqlSession.insert(statement, dto);
+	}
+	public int goodsNum() {
+		statement = namespace + ".goodsNum";
+		return sqlSession.selectOne(statement);
+	}
+}
---
+++ SpringMybatisProject/src/main/java/service/goods/GoodsNumberService.java
@@ -0,0 +1,15 @@
+package service.goods;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+
+import repository.GoodsRepository;
+
+public class GoodsNumberService {
+	@Autowired
+	GoodsRepository goodsRepository;
+	public void goodsNum(Model model) {
+		int goodsNum = goodsRepository.goodsNum();
+		model.addAttribute("goodsNum", goodsNum);
+	}
+}
